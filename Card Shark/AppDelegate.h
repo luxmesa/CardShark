9d16ace1ebbd2784263f567ca0a1346e77e4bf42
Samuel Vercauteren <samuelvercauteren@Samuels-iMac.local>
2017-04-09 13:17:10 -0500

    Initial Commit

---
+++ Card Shark/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Card Shark
+//
+//  Created by Samuel Vercauteren on 4/9/17.
+//  Copyright © 2017 Samuel Vercauteren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
